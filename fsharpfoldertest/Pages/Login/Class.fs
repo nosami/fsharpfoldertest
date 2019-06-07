b51cb31c774dc16751cc505060f65363154f6167
nosami <jasonimison@gmail.com>
2019-06-07 13:38:14 +0100

    stash

--- fsharpfoldertest/Pages/Login/Class.fs
+++ fsharpfoldertest/Pages/Login/Class.fs
@@ -1,3 +1,3 @@
 ﻿module x =
     let z = 1
-    z
+   
--- fsharpfoldertest/Program.fs
+++ fsharpfoldertest/Program.fs
@@ -1,6 +1,6 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-[<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+﻿namespace whatever
+module test =
+
+    let main argv =
+        printfn "%A" argv
+        0 // return an integer exit code
